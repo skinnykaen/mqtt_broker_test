79e705b8954a24e430ad67d6599752fd9d43367e
skinnykaen <rupychman@mail.ru>
2021-02-11 20:32:50 +0300

    add useState

--- job_test/src/Pages/Login.js
+++ job_test/src/Pages/Login.js
@@ -2,14 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './Styles/Login.css';
 
+
+
 export default class Login extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
+    this.state = {
+      email: '',
+      password: ''
+    };
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+
   }
 
+  handleEmailChange(e) {
+    this.setState({ email: e.target.value })
+  }
+  handlePasswordChange(e) {
+    this.setState({ password: e.target.value })
+  }
+
+  signIn() {
+    alert('Email address is ' + this.state.email + ' Password is ' + this.state.password);
+  }
+  
   render() {
-    return(
+    return (
       <div className="login">
         <div className="wrapper-title">
           <div className="title">
@@ -18,31 +38,31 @@ export default class Login extends React.Component {
         </div>
 
         <div className="wrapper-login" id="login">
-            <div className="top-elem">
-              <div className="input-group mb-3">
-                <span className="input-group-text" id="mailLabel">E-mail</span>
-                <input id="mail" name="mail" type="text" className="form-control" placeholder="exemple@mail.ru" aria-label="Email" aria-describedby="mailLabel" />
-              </div>
+          <div className="top-elem">
+            <div className="input-group mb-3">
+              <span className="input-group-text" id="mailLabel">E-mail</span>
+              <input id="email" name="email" type="email" onChange={this.handleEmailChange} className="form-control" placeholder="exemple@mail.ru" aria-label="Email" aria-describedby="mailLabel" />
+            </div>
 
-              <div class="mb-3 row">
-                <div className="input-group mb-3">
+            <div class="mb-3 row">
+              <div className="input-group mb-3">
                 <span className="input-group-text" id="passwordLabel">Пароль</span>
-                <input id="password" name="password" type="text" className="form-control" placeholder="" aria-label="Password" aria-describedby="passwordLabel" />
+                <input id="password" name="password" type="password" onChange={this.handlePasswordChange} className="form-control" placeholder="" aria-label="Password" aria-describedby="passwordLabel" />
               </div>
             </div>
-      
+
             <div className="bottom-elem">
               <div className="forgot-password">
                 <a className="link" href="#">Забыли пароль?</a>
               </div>
 
               <div className="send-button-wrapper">
-                <button type="submit" className="btn btn-primary">Войти</button>
+                <button type="submit" onClick={this.signIn} className="btn btn-primary">Войти</button>
               </div>
             </div>
+          </div>
         </div>
       </div>
-    </div>
     );
   }
 }
